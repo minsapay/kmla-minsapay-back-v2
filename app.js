b7f15d37b1eaf584e6ca148535e3df161d897592
Jungwoong Kim <dk040106@naver.com>
2020-08-10 15:10:52 +0900

    erase: public file not used

--- app.js
+++ app.js
@@ -29,9 +29,6 @@ app.use(
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  express.static(path.join(__dirname, './static'), { dotfiles: 'allow' })
-);
 
 app.use('/booth', boothRouter);
 app.use('/student', studentRouter);
