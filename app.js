48ac4fd1753103d5d792f457d4e96454933a22cd
Jungwoong Kim <dk040106@naver.com>
2020-08-10 15:01:25 +0900

    add: static service for ssl

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  express.static(path.join(__dirname, './static'), { dotfiles: 'allow' })
+);
 
 app.use('/booth', boothRouter);
 app.use('/student', studentRouter);
